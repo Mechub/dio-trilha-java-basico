1cc80d6cce69c64767aa372bd5412bbaf8d2b018
Mercio franca <mercioalbuquerque@hotmail.com>
2023-08-30 12:34:09 -0300

    pequena mudança de posição do "sc.nextline()"

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -4,22 +4,23 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Por favor, digite o número da Agência!: ");
+        int numeroAgencia = sc.nextInt();
 
-    System.out.println("Por favor, digite o número da Agência!");
-    int numeroAgencia = sc.nextInt();
+        System.out.println("Por favor, digite a Agência!: ");
+        String agencia = sc.next();
 
-    System.out.println("Por favor, digite a Agência!");
-    String agencia = sc.next();
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String cliente = sc.next();
+        sc.nextLine();
 
-    System.out.println("Por favor, digite o nome do Cliente");
-    String cliente = sc.next();
+        System.out.println("Saldo: ");
+        double saldo = sc.nextDouble();
 
-    System.out.println("Saldo: ");
-    double saldo = sc.nextDouble();
-    sc.nextLine();
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+                cliente, agencia, numeroAgencia, saldo);
 
-    System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", cliente, agencia, numeroAgencia, saldo );
-
-
-    sc.close();
- }}
+        sc.close();
+    }
+}
